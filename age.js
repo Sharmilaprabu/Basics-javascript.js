97ea45dcd32b609a774ff30488e8809d92bcffe1
sharmila Prabu <132900368+Sharmilaprabu@users.noreply.github.com>
2024-09-06 23:12:53 +0530

    Add files via upload

---
+++ age.js
@@ -0,0 +1,19 @@
+let age = 20;
+
+
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}
+
+
+if (age >= 65) {
+    console.log("You are a senior citizen.");
+} else if (age >= 40) {
+    console.log("You are in your middle age.");
+} else if (age >= 18) {
+    console.log("You are a young adult.");
+} else {
+    console.log("You are a child or teenager.");
+}
